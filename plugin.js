2cb2329bd38cb9749bd0331ace9569f87e50eb4e
Matteo Collina <hello@matteocollina.com>
2022-12-23 18:43:02 +0100

    Actually calling destinations
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- plugin.js
+++ plugin.js
@@ -1,17 +1,92 @@
 /// <reference path="./global.d.ts" />
 'use strict'
 
+const { request } = require('undici')
+
 /** @param {import('fastify').FastifyInstance} app */
 module.exports = async function (app) {
+  let timer = null
+  let nextTime = -1
 
   app.platformatic.addEntityHooks('item', {
     async save (original, { input, ...rest }) {
       if (!input.when) {
-        input.when = (new Date()).toISOString() // now
+        input.when = new Date().getTime() // now
+      }
+
+      const date = new Date(input.when)
+      if ((nextTime > 0 && date.getTime() < nextTime) || !timer) {
+        clearTimeout(timer)
+        nextTime = date.getTime()
+        const delay = nextTime - Date.now()
+        timer = setTimeout(execute, delay)
       }
 
       return original({ input, ...rest })
     }
   })
 
+  async function execute () {
+    const now = Date.now()
+    const items = await app.platformatic.entities.item.find({
+      where: {
+        when: {
+          lte: now
+        },
+        sentAt: {
+          eq: null
+        }
+      }
+    })
+
+    const [next] = await app.platformatic.entities.item.find({
+      where: {
+        when: {
+          gt: now
+        },
+        sentAt: {
+          eq: null
+        }
+      },
+      limit: 1
+    })
+
+    for (const item of items) {
+      const { callbackUrl, method, body } = item
+      try {
+        const res = await request(callbackUrl, {
+          method,
+          headers: {
+            'content-type': 'application/json'
+          },
+          body
+        })
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          await app.platformatic.entities.item.save({
+            input: {
+              id: item.id,
+              sentAt: new Date().getTime()
+            }
+          })
+          // TODO try again if it fails
+        }
+        app.log.log({ callbackUrl, method }, 'callback!')
+      } catch (err) {
+        app.log.error(err)
+      }
+    }
+
+    if (next) {
+      const delay = next.when - now
+      timer = setTimeout(execute, delay)
+    }
+  }
+
+  await execute()
+
+  app.addHook('onClose', () => {
+    if (timer) {
+      clearTimeout(timer)
+    }
+  })
 }
--- test.js
+++ test.js
@@ -5,6 +5,15 @@ const { buildServer } = require('@platformatic/db')
 const { join } = require('path')
 const { readFile, rm } = require('fs/promises')
 const { tmpdir } = require('os')
+const { once, EventEmitter } = require('events')
+const Fastify = require('fastify')
+
+const { setGlobalDispatcher, Agent } = require('undici')
+
+setGlobalDispatcher(new Agent({
+  keepAliveTimeout: 10,
+  keepAliveMaxTimeout: 10
+}))
 
 let count = 0
 
@@ -12,7 +21,7 @@ function getFilename () {
   return join(tmpdir(), `test-${process.pid}-${count++}.db`)
 }
 
-test('storing a queue and an item', async ({ teardown, equal }) => {
+async function getConfig () {
   const config = JSON.parse(await readFile(join(__dirname, './platformatic.db.json'), 'utf8'))
   config.server.port = 0
   config.server.logger = false
@@ -20,10 +29,27 @@ test('storing a queue and an item', async ({ teardown, equal }) => {
   config.core.connectionString = `sqlite://${filename}`
   config.migrations.autoApply = true
   config.types.autogenerate = false
+  return { config, filename }
+}
 
+test('happy path', async ({ teardown, equal, plan, same }) => {
+  plan(5)
+  const ee = new EventEmitter()
+  const { config, filename } = await getConfig()
   const server = await buildServer(config)
   teardown(() => server.stop())
   teardown(() => rm(filename))
+
+  const target = Fastify()
+  target.post('/', async (req, reply) => {
+    same(req.body, { message: 'HELLO FOLKS!' }, 'message is equal')
+    ee.emit('called')
+    return { ok: true }
+  })
+
+  teardown(() => target.close())
+  await target.listen({ port: 0 })
+  const targetUrl = `http://${target.server.address().address}:${target.server.address().port}`
 
   let queueId
   {
@@ -47,25 +73,37 @@ test('storing a queue and an item', async ({ teardown, equal }) => {
     equal(queueId, '1')
   }
 
+  const p = once(ee, 'called')
   {
+    const msg = JSON.stringify({
+      message: 'HELLO FOLKS!'
+    })
     const now = Date.now()
     const res = await server.app.inject({
       method: 'POST',
       url: '/graphql',
       payload: {
         query: `
-          mutation {
-            saveItem(input: { queueId: ${queueId}, callbackUrl: "http://localhost:3000", method: "POST", body: "HELLO FOLKS!" }) { id
+          mutation($body: String!, $queueId: ID, $callbackUrl: String!) {
+            saveItem(input: { queueId: $queueId, callbackUrl: $callbackUrl, method: "POST", body: $body }) {
+              id
               when
             }
           }
-        `
+        `,
+        variables: {
+          body: msg,
+          queueId,
+          callbackUrl: targetUrl
+        }
       }
     })
-    equal(res.statusCode, 200)
     const body = res.json()
+    equal(res.statusCode, 200)
     const { data } = body
     const when = new Date(data.saveItem.when)
     equal(when.getTime() - now >= 0, true)
   }
+
+  await p
 })
