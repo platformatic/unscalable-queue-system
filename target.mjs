3ad7f6e2bd317bb860fa7bebef7f08c13c488332
Matteo Collina <hello@matteocollina.com>
2023-01-12 18:54:44 +0100

    added simple target
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ target.mjs
@@ -0,0 +1,16 @@
+import fastify from "fastify"
+
+const app = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty'
+    }
+  }
+})
+
+app.post('/', async (request, reply) => {
+  request.log.info({ body: request.body }, 'request body')
+  return { status: 'ok' }
+})
+
+await app.listen({ port: 3000 })
