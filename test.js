bb92c129ce39c52589b74fae04bba3a040ea5dca
Matteo Collina <hello@matteocollina.com>
2023-01-16 18:11:34 +0100

    Add test for callbacks in the future (#4)
    * Add test for callbacks in the future
    
    * run tests in CI
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    * tests passing on Node v18
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test.js
+++ test.js
@@ -49,7 +49,7 @@ test('happy path', async ({ teardown, equal, plan, same }) => {
 
   teardown(() => target.close())
   await target.listen({ port: 0 })
-  const targetUrl = `http://${target.server.address().address}:${target.server.address().port}`
+  const targetUrl = `http://localhost:${target.server.address().port}`
 
   let queueId
   {
@@ -128,7 +128,7 @@ test('`text plain` content type', async ({ teardown, equal, plan, same }) => {
 
   teardown(() => target.close())
   await target.listen({ port: 0 })
-  const targetUrl = `http://${target.server.address().address}:${target.server.address().port}`
+  const targetUrl = `http://localhost:${target.server.address().port}`
 
   let queueId
   {
@@ -183,4 +183,87 @@ test('`text plain` content type', async ({ teardown, equal, plan, same }) => {
   }
 
   await p
+})
+
+test('future when', async ({ teardown, equal, plan, same }) => {
+  plan(6)
+
+  const ee = new EventEmitter()
+  const { config, filename } = await getConfig()
+  const server = await buildServer(config)
+  teardown(() => server.stop())
+  teardown(() => rm(filename))
+
+  const target = Fastify()
+  target.post('/', async (req, reply) => {
+    same(req.body, { message: 'HELLO FOLKS!' }, 'message is equal')
+    ee.emit('called', Date.now())
+    return { ok: true }
+  })
+
+  teardown(() => target.close())
+  await target.listen({ port: 0 })
+  const targetUrl = `http://localhost:${target.server.address().port}`
+
+  let queueId
+  {
+    const res = await server.app.inject({
+      method: 'POST',
+      url: '/graphql',
+      payload: {
+        query: `
+          mutation {
+            saveQueue(input: { name: "test" }) {
+              id
+            }
+          }
+        `
+      }
+    })
+    equal(res.statusCode, 200)
+    const body = res.json()
+    const { data } = body
+    queueId = data.saveQueue.id
+    equal(queueId, '1')
+  }
+
+  const p = once(ee, 'called')
+  const now = Date.now()
+
+  {
+    const msg = JSON.stringify({
+      message: 'HELLO FOLKS!'
+    })
+    const afterOneSecond = new Date(now + 1000).toISOString()
+    const query = `
+      mutation($body: String!, $queueId: ID, $callbackUrl: String!, $when: DateTime!) {
+        saveItem(input: { queueId: $queueId, callbackUrl: $callbackUrl, method: "POST", headers: "{ \\"content-type\\": \\"application/json\\" }", body: $body, when: $when  }) {
+          id
+          when
+        }
+      }
+    `
+
+    const res = await server.app.inject({
+      method: 'POST',
+      url: '/graphql',
+      payload: {
+        query,
+        variables: {
+          body: msg,
+          queueId,
+          callbackUrl: targetUrl,
+          when: afterOneSecond
+        }
+      }
+    })
+    const body = res.json()
+    equal(res.statusCode, 200)
+
+    const { data } = body
+    equal(data.saveItem.when, afterOneSecond)
+  }
+
+  const [calledAt] = await p
+  equal(calledAt - now >= 1000, true)
 })
