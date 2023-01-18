3ab17c48344b2b6c98c820ca457ad225d6b103d1
Matteo Collina <hello@matteocollina.com>
2023-01-18 18:03:46 +0100

    computeBackoff should not throw
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/backoff.js
+++ test/backoff.js
@@ -37,4 +37,7 @@ test('computeBackoff', async ({ teardown, same, plan }) => {
   same(computeBackoff({ retries: 0, maxRetries: 5 }), { retries: 1, maxRetries: 5, waitFor: 100 })
   same(computeBackoff({ retries: 1, maxRetries: 5 }), { retries: 2, maxRetries: 5, waitFor: 200 })
   same(computeBackoff({ retries: 2, maxRetries: 5 }), { retries: 3, maxRetries: 5, waitFor: 400 })
+  same(computeBackoff({ retries: 3, maxRetries: 5 }), { retries: 4, maxRetries: 5, waitFor: 800 })
+  same(computeBackoff({ retries: 4, maxRetries: 5 }), { retries: 5, maxRetries: 5, waitFor: 1600 })
+  same(computeBackoff({ retries: 5, maxRetries: 5 }), false)
 })
